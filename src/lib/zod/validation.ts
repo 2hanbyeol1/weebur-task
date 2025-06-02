ecaccd77871fd2f1aba1c132583bc916ca87694e
2hanbyeol1 <2hanbyeol1@naver.com>
2025-06-03 05:16:23 +0900

    fix: zod 설정 변경

--- src/lib/zod/validation.ts
+++ src/lib/zod/validation.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 
 export const createProductSchema = z.object({
-  title: z.string().nonempty("상품명을 입력해 주세요").max(15),
+  title: z
+    .string()
+    .trim()
+    .nonempty("상품명을 입력해 주세요")
+    .max(15, "15자 이내로 입력해 주세요"),
   description: z.string(),
-  price: z.coerce
-    .number()
-    .nonnegative("0원부터 입력할 수 있어요")
-    .gte(1000, "1000원부터 입력할 수 있어요"),
+  price: z.coerce.number().gte(1000, "1000원부터 입력할 수 있어요"),
   discountPercentage: z.coerce
     .number()
     .nonnegative("0%부터 입력할 수 있어요")
